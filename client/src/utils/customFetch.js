31f7073b90c504b033d1bc7dcd9fdbffad77e8ab
Joseph Sun <155233774+josephsun22@users.noreply.github.com>
2025-07-11 14:38:00 +0930

    set workflows

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
-const customFetch = axios.create({
-  baseURL: '/api/v1',
-});
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? '/api/v1'
+    : `${import.meta.env.VITE_API_URL}/api/v1`;
+
+    const customFetch = axios.create({
+      baseURL,
+    });
 
 export default customFetch;
